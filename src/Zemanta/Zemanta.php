c07dc243e257b128b67edb9feabca9263e3ce290
Marcelo Rodrigues <marcelo.mx@gmail.com>
2013-08-28 14:54:51 -0300

    Added LICENCE. Api method and request implemented. Parse JSON response implemented

--- src/Zemanta/Zemanta.php
+++ src/Zemanta/Zemanta.php
@@ -7,7 +7,14 @@ class Zemanta
 	/**
 	 * Default URL endpoint to Zemanta REST API
 	 */
-	const END_POINT = 'api.zemanta.com/services/rest/0.0';
+	const END_POINT = 'http://api.zemanta.com/services/rest/0.0';
+
+	/**
+	 * API methods
+	 */
+	const METHOD_SUGGEST        = 'zemanta.suggest';
+	const METHOD_SUGGEST_MARKUP = 'zemanta.suggest_markup';
+	const METHOD_PREFERENCES    = 'zemanta.preferences';
 
 	/**
 	 * @var string The api key provided by Zemanta service
@@ -24,6 +31,14 @@ class Zemanta
 	{
 		$this->apiKey = $apiKey;		
 		$this->endPoint = $endPoint ? $endPoint : self::END_POINT;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEndPoint()
+	{
+		return $this->endPoint;
 	}
 
 	/**
@@ -51,6 +66,113 @@ class Zemanta
 	 */
 	public function api()
 	{
-		// TODO
+		// Map arguments
+		$parameters = array();
+		$args  = func_get_args();
+
+		switch (func_num_args()) {
+			case 3:
+				$parameters = $args[2];
+				$parameters['text']  == $args[1];				
+				$parameters['method'] = $args[0];
+				break;
+			case 2:
+				$parameters = $args[1];			
+				$parameters['method'] = $args[0];
+				break;
+			case 1:
+				$parameters = $args[0];
+				break;
+			case 0:
+			default: 
+				throw new \InvalidArgumentException('No parameters to build query from');
+		}
+
+		// Set default return format. It must be necessary to parse raw response
+		if (!isset($parameters['format'])) {
+			$parameters['format'] = 'xml';
+		}		
+		$response  = $this->getRaw($parameters);
+		$returnValue = array();
+
+		if ($parameters['format'] == 'json') {
+			$returnValue = json_decode($response, true);
+		}
+
+		// // Parse response
+		// switch ($parameters['format']) {
+		// 	case 'rdf':
+		// 	case 'xml':
+		// 		break;
+		// 	case 'wdjson':
+		// 	case 'json':
+		// 		$this->parseJSON($response);
+		// }
+
+
+		return $returnValue;
+	}
+
+	/**
+	 * Request API method, format other than XML. 
+	 * Other supported formats: JSON, WJSON, RDF/XML
+	 *
+	 * The argument must be an array with method and other
+     *
+     * @param array $parameters
+     *
+     * @return string
+	 */
+	public function getRaw($parameters = array())
+	{
+		// Validating method
+		if (!isset($parameters['method'])) {
+			throw new \InvalidArgumentException('No method to request');			
+		}
+
+		// Validating method text
+		if ($parameters['method'] == static::METHOD_SUGGEST || $parameters['method'] == static::METHOD_SUGGEST_MARKUP) {
+			if (!isset($parameters['text'])) {
+				throw new \InvalidArgumentException('No text to analyse');
+			}
+		}
+		$parameters['api_key'] = $this->apiKey;		
+
+		// Set default return format
+		if (!isset($parameters['format'])) {
+			$parameters['format'] = 'xml';
+		}
+
+		// Validating format
+		if (!preg_match("/^(xml|json|wnjson|rdfxml)$/", $parameters['format'])) {
+			throw new \InvalidArgumentException(sprintf( 'Format %s is not supported', $parameters['format']) );
+		}
+
+		// Preparing and make request
+		$response = $this->makeRequest($this->endPoint, $parameters);
+
+		return $response;
+	}
+
+	/**
+	 * @param string $url
+	 *
+	 * @return string
+	 */
+	protected function makeRequest($url, $params = array())
+	{		
+		$content  = http_build_query($params);		
+		$context = stream_context_create(array(
+			'http' => array(
+				'method' => 'POST',
+				'header' => "Content-type: application/x-www-form-urlencoded\r\n"
+				  		  . "Content-length: " . strlen($content) . "\r\n",
+				'content' => $content
+			)
+		));
+
+		$result = file_get_contents($url, false, $context);
+
+		return $result;
 	}
 }
--- tests/Zemanta/ZemantaTest.php
+++ tests/Zemanta/ZemantaTest.php
@@ -5,6 +5,7 @@ namespace Zemanta;
 class ZemantaTest extends \PHPUnit_Framework_TestCase
 {
 	protected $apiKey;
+	protected $defaultParams;
 
 	public function setUp()
 	{
@@ -24,6 +25,7 @@ class ZemantaTest extends \PHPUnit_Framework_TestCase
 		$zemanta = new Zemanta($this->apiKey);
 
 		$this->assertAttributeEquals($endPoint, 'endPoint', $zemanta);
+		$this->assertEquals($endPoint, $zemanta->getEndPoint());
 	}
 
 	public function testEndPointCustomized()
@@ -32,10 +34,120 @@ class ZemantaTest extends \PHPUnit_Framework_TestCase
 		$zemanta = new Zemanta($this->apiKey, $endPoint);
 
 		$this->assertAttributeEquals($endPoint, 'endPoint', $zemanta);
+		$this->assertEquals($endPoint, $zemanta->getEndPoint());
+	}
+
+	public function testGetRawInvalidParametersNoMethod() 
+	{
+		$this->setExpectedException('\InvalidArgumentException');
+		$zemanta = new Zemanta($this->apiKey);
+		$zemanta->getRaw(array());
+	}
+
+	public function testGetRawInvalidParametersNoText() 
+	{
+		$this->setExpectedException('\InvalidArgumentException');
+		$zemanta = new Zemanta($this->apiKey);
+		$zemanta->getRaw(array('method' => Zemanta::METHOD_SUGGEST));
+	}
+
+	public function testGetRawInvalidFormat()
+	{
+		$this->setExpectedException('\InvalidArgumentException');
+		$zemanta = new Zemanta($this->apiKey);
+		$zemanta->getRaw(array('method' => Zemanta::METHOD_PREFERENCES, 'format' => 'asp'));
+	}
+
+	public function testGetRaw()
+	{
+		$params = array(
+			'method' => Zemanta::METHOD_SUGGEST, 
+			'text'   => 'Lorem ipsum'			
+		);		
+		$requestParams = array_merge($params, array('api_key' => $this->apiKey, 'format' => 'xml'));
+
+		$requestUrl = Zemanta::END_POINT;
+		$expectedResponse = '<rsp><status>ok</status></rsp>';
+
+		$zemanta = $this->getMock('\Zemanta\Zemanta', array('makeRequest'), array($this->apiKey));
+		$zemanta->expects($this->once())
+				->method('makeRequest')
+				->with($requestUrl, $requestParams)
+				->will($this->returnValue($expectedResponse));
+
+		$response = $zemanta->getRaw($params);
+
+		$this->assertInternalType('string', $response);
+		$this->assertEquals($expectedResponse, $response);
 	}
 
 	public function testApiInvalidArguments()
 	{
-		$zemanta = new Zemanta($this->apiKey);
+		$this->setExpectedException('\InvalidArgumentException');
+		$zemanta = new Zemanta($this->apiKey);		
+		$zemanta->api();
+	}	
+
+	public function testApi()
+	{
+		$params = array(
+			'method' => Zemanta::METHOD_SUGGEST, 
+			'text'   => 'Lorem ipsum',
+			'format' => 'xml'
+		);
+		$zemanta = $this->getMock('\Zemanta\Zemanta', array('getRaw'), array($this->apiKey));
+		$zemanta->expects($this->once())
+				->method('getRaw')
+				->with($this->equalTo($params));
+
+		unset($params['format']);
+
+		$response = $zemanta->api($params);
+
+		$this->assertInternalType('array', $response);
+	}
+
+	public function testApiMethodArgument()
+	{
+		$params = array(
+			'method' => Zemanta::METHOD_PREFERENCES, 
+			'text'   => 'Lorem ipsum',
+			'format' => 'xml'
+		);				
+		$zemanta = $this->getMock('\Zemanta\Zemanta', array('getRaw'), array($this->apiKey));
+		$zemanta->expects($this->once())
+				->method('getRaw')
+				->with($this->equalTo($params));
+
+		$response = $zemanta->api(Zemanta::METHOD_PREFERENCES, 'text', $params);
+
+		$this->assertInternalType('array', $response);
+	}	
+
+	// Test parse response XML, WJSON, JSON, RDF
+	public function testApiParseXML()
+	{		
+		$params = array(
+			'method' => Zemanta::METHOD_SUGGEST, 
+			'text'   => 'Lorem ipsum',
+			'format' => 'json'
+		);						
+		$json = $this->getResponseFixture('suggest.json');		
+
+		$zemanta = $this->getMock('\Zemanta\Zemanta', array('makeRequest'), array($this->apiKey));
+		$zemanta->expects($this->once())
+				->method('makeRequest')
+				->will($this->returnValue($json));
+				
+		$response = $zemanta->api($params);
+
+		$this->assertInternalType('array', $response);
+		$this->assertArrayHasKey('status', $response);
+		$this->assertArrayHasKey('articles', $response);
+	}	
+
+	protected function getResponseFixture($file)
+	{
+		return file_get_contents(__DIR__ . '/Resources/' . $file);
 	}
 }
